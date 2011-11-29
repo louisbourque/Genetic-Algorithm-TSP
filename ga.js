99569fbf9aaa4c38eff762571674dbc32e41a7e8
Louis Bourque <louisrbourque@gmail.com>
2011-11-29 11:42:07 -0500

    separate js from html

--- ga.js
+++ ga.js
@@ -1,357 +1,211 @@
-//import scripts:
-//json2.js is an open source JSON parser, available at http://www.json.org/js.html
-//json2.js is used to convert objects to string when passing to/from the worker
-importScripts('json2.js');
-//postMessage('{"act":"debug","data":"message"}');
+var ctx;
 
-/**
-This is the worker. It is used by a1.html to perform all the CPU-intensive
-processing, so the GUI will remain responsive.
-**/
+//config object used to set the parameters of the game. This object is passed to the worker thread to initialize it
+var config = new Object();
+config.popSize = 50;
+config.maxGenerations = 35;
+config.maxRuns = 1;
+config.mutateProb = 0.02;
+config.selection = "rank";
+config.fitness_order = "asc";
+config.unique_chromosomes = false;
+var worker;
 
-var config;
-var runTimeout = 0;
-var stop_running = true;
-var population;
-var run;
-var gen;
-var measure_fitness;
 
-//this is the function that is called whenever the worker receives a message.
-//based on the content of the message (event.data.act), do the appropriate action.
-onmessage = function(event) {
-	var message = JSON.parse(event.data);
-	switch(message.act){
-		case 'pause':
-			stop_running = true;
-			if(runTimeout) clearTimeout(runTimeout);
-			break;
-		case 'init':
-			config = message.data;
-			switch(config.fitness_alg){
-				case "one_max":
-					measure_fitness = one_max_fitness;
-					break;
-				case "simple_max":
-					measure_fitness = simple_max_fitness;
-					break;			
-				case "tsp":
-					measure_fitness = tsp_fitness;
-					break;	
-			}
-			run = 0;
-			runGA();
-			break;
-		case "test_crossover":
-			var result = new Object();
-			result.a1 = crossover1(message.data.parent1,message.data.parent2);
-			result.a2 = crossover2(message.data.parent1,message.data.parent2);
-			var message = new Object();
-			message.act = "test_crossover";
-			message.data = result;
-			postMessage(JSON.stringify(message));
-	}
-}
-
-function runGA(){
-
-	gen = 0;
-	//make initial random population
-	population = new Array();
-	for(var i = 0;i<config.popSize;){
-		var object = new Object();
-		object.chromosome = generate_chromosome();
-		object.fitness = 0;
-		if(insert_into_population(object,population))
-			i++;
-	}
-	stop_running = false;
-	iterGA();
-}
-
-function iterGA(){
-	//sort population by fitness
-	if(config.fitness_order == "desc")
-		population.sort( function (a,b) { return b.fitness-a.fitness });
-	else
-		population.sort( function (a,b) { return a.fitness-b.fitness });
+function init(){
+	ctx = document.getElementById('canvas').getContext("2d");
+	worker = new Worker("ga-worker.js");
+	worker.onerror = function(error) {  
+		console.log(error.message);
+	};
 	
+}
 
-	//report best so far
+//start the run loop
+function one_max_init(){
+	stop();
+	config.fitness_alg = "one_max";
+	config.fitness_order = "asc";
+	config.chromosome_length = 50;
+	config.maxGenerations = 35;
+	config.chars = ['0','1'];
+	config.unique_chromosomes = false;
+	config.selection = $('#selection').val();
+	$('#result').empty();
+	$('#canvas').hide();
+	
+	worker.onmessage = function(event) {
+		handle_worker_message_onemax(event.data);
+	};
 	var message = new Object();
-	message.act = "generation";
-	message.gen = gen;
-	message.data = population[population.length-1];
-	postMessage(JSON.stringify(message));
+	message.act = "init";
+	message.data = config;
+	worker.postMessage(JSON.stringify(message));
+}
+
+function simple_max_init(){
+	stop();
+	config.fitness_alg = "simple_max";
+	config.fitness_order = "asc";
+	config.chromosome_length = 10;
+	config.maxGenerations = 35;
+	config.chars = [1,2,3,4,5,6,7,8,9,10];
+	config.unique_chromosomes = false;
+	config.selection = $('#selection').val();
+	$('#result').empty();
+	$('#canvas').hide();
 	
-	//termination sat for run?
-	if(stop_running || config.maxGenerations == gen){
-		run++;
-		var message = new Object();
-		message.act = "answer";
-		message.data = population[population.length-1];
-		postMessage(JSON.stringify(message));
-		if(!stop_running && run < config.maxRuns){
-			runTimeout = setTimeout(runGA, 150);
-			return true;
-		}
+	worker.onmessage = function(event) {
+		handle_worker_message_simplemax(event.data);
+	};
+	var message = new Object();
+	message.act = "init";
+	message.data = config;
+	worker.postMessage(JSON.stringify(message));
+}
+
+function tsp_init(){
+	stop();
+	$.get($('#tsp_map').val()+'.txt', function(data){
+  		config.fitness_alg = "tsp";
+  		config.fitness_order = "desc";
+		config.unique_chromosomes = true;
+  		config.chars = new Array();
+  		config.cities = new Object();
+  		config.popSize = 250;
+  		config.maxGenerations = 1000;
+  		config.selection = $('#selection').val();
+  		var lines=data.split("\n");
+  		for(var i=0; i<lines.length; i++) {
+  			var parts = lines[i].split(' ');
+  			if(parts.length != 3)
+  				continue;
+  			config.chars.push(parts[0]);
+  			var city = new Object();
+  			city.x = parts[1];
+  			city.y = parts[2];
+  			config.cities[parts[0]] = city;
+  		}
+		//number of cities
+		config.chromosome_length = config.chars.length;
 		
+		$('#result').empty();
+		$('#canvas').show();
+		draw_cities();
+		
+		worker.onmessage = function(event) {
+			handle_worker_message_tsp(event.data);
+		};
+		var message = new Object();
+		message.act = "init";
+		message.data = config;
+		worker.postMessage(JSON.stringify(message));
+	});
+}
+
+
+function handle_worker_message_onemax(data){
+	var resultObj = JSON.parse(data);
+	if(resultObj.act == "debug"){
+		console.log(resultObj.data);
+		return false;
+	}
+	if(resultObj.act == "generation" && resultObj.gen){
+		$('#result').prepend("Best individual of generation "+resultObj.gen+": "+resultObj.data.chromosome+" ("+resultObj.data.fitness+")<br>");
 		return true;
 	}
-	
-	for(var i = 0;i<population.length;i++){
-		population[i].fitness = measure_fitness(population[i].chromosome);
-	}
-	
-	
-	var newPopulation = new Array();
-	for(var i = 0;i<population.length;){
-		var rnum = Math.ceil(Math.random() * 3);
-		switch(rnum){
-			case 1:
-				//select one individual based on fitness
-				var individual = population[select_from_population()];
-				//perform reproduction
-				var newIndividual = new Object();
-				newIndividual.chromosome = individual.chromosome.slice();
-				newIndividual.fitness = individual.fitness;
-				//insert copy in new pop
-				if(insert_into_population(newIndividual,newPopulation))
-					i++;
-				break;
-			case 2:
-				//select two individuals based on fitness
-				var individual1 = population[select_from_population()];
-				var individual2 = population[select_from_population()];
-				//perform crossover
-				var child1 = new Object();
-				var child2 = new Object();
-				var xover = Math.floor(Math.random()*individual1.chromosome.length);
-				if(config.unique_chromosomes){
-					var r = Math.random();
-					if(r < 0.5){
-						child1.chromosome = crossover1(individual1.chromosome,individual2.chromosome);
-						child2.chromosome = crossover1(individual1.chromosome,individual2.chromosome);
-					}else{
-						child1.chromosome = crossover2(individual1.chromosome,individual2.chromosome);
-						child2.chromosome = crossover2(individual1.chromosome,individual2.chromosome);						
-					}
-				}else{
-					child1.chromosome = individual1.chromosome.slice(0,xover).concat(individual2.chromosome.slice(xover));
-					child2.chromosome = individual2.chromosome.slice(0,xover).concat(individual1.chromosome.slice(xover));
-				}
-				child1.fitness = measure_fitness(child1.chromosome);
-				child2.fitness = measure_fitness(child2.chromosome);
-				
-				var candidates = new Array();
-				candidates.push(individual1);
-				candidates.push(individual2);
-				candidates.push(child1);
-				candidates.push(child2);
-				if(config.fitness_order == "desc")
-					candidates.sort( function (a,b) { return b.fitness-a.fitness });
-				else
-					candidates.sort( function (a,b) { return a.fitness-b.fitness });
-				//insert offspring in new pop
-				if(insert_into_population(candidates[2],newPopulation))
-					i++;
-				if(insert_into_population(candidates[3],newPopulation))
-					i++;
-				break;
-			case 3:
-				//select one individial based on fitness
-				var individual = population[select_from_population()];
-				//perform mutation
-				var mutant = new Object();
-				mutant.chromosome = individual.chromosome.slice();
-				var r = Math.random();
-				var x1 = Math.floor(Math.random()*mutant.chromosome.length);
-				var x2 = Math.floor(Math.random()*mutant.chromosome.length);
-				if(r < 0.5){
-					//Mutate 1 - reciprocal exchange
-					var temp = mutant.chromosome[x1];
-					mutant.chromosome[x1] = mutant.chromosome[x2];
-					mutant.chromosome[x2] = temp;
-				}else{
-					//Mutate 2 - insertion
-					var tempC = mutant.chromosome.splice(x1,1);
-					var tempA = mutant.chromosome.splice(x2);
-					mutant.chromosome = mutant.chromosome.concat(tempC.concat(tempA));
-				}
-				mutant.fitness = measure_fitness(mutant.chromosome);
-				//insert mutant in new pop
-				if(insert_into_population(mutant,newPopulation))
-					i++;
-				break;
-			default:
-		}
-		
-	}
-	population = newPopulation;
-	
-	gen++;
-	
-	if(!stop_running){
-		runTimeout = setTimeout(iterGA, 50);
+	if(resultObj.act == "answer"){
+		$('#result').prepend("Answer: "+resultObj.data.chromosome+"<br>With a fitness of "+resultObj.data.fitness+"<br>");
+		return true;
 	}
 }
-
-function crossover1(parent1, parent2){
-	//Order crossover
-	var A = Math.floor(Math.random()*parent1.length);
-	var B = Math.floor(Math.random()*parent1.length);
-	while(A == B)
-		B = Math.floor(Math.random()*parent1.length);
-	if(A > B){
-		var temp = B;
-		B = A;
-		A = temp;
-	}
-	var child = new Array(parent1.length);
-	//copy A to B from parent 1
-	for(var i = A;i<B;i++){
-		child[i] = parent1[i];
-	}
-	//fill in the rest of child with parent2's genes
-	var parent2_index = 0;
-	for(var child_index = 0;child_index<parent1.length;child_index++){
-		if(child[child_index] == undefined){
-			for(;parent2_index<parent1.length;parent2_index++){
-				if(child.indexOf(parent2[parent2_index])<0){
-					child[child_index] = parent2[parent2_index];
-					break;
-				}
-			}
-		}
-	}
-	return child;
-}
-
-function crossover2(parent1, parent2){
-	//Position-based crossover
-	var child = new Array(parent1.length);
-	for(var i = 0;i<parent1.length;i++){
-		var r = Math.random();
-		if(r < 0.5)
-			child[i] = parent1[i];
-	}
-	//fill in the rest of child with parent2's genes
-	var parent2_index = 0;
-	for(var child_index = 0;child_index<parent1.length;child_index++){
-		if(child[child_index] == undefined){
-			for(;parent2_index<parent1.length;parent2_index++){
-				if(child.indexOf(parent2[parent2_index])<0){
-					child[child_index] = parent2[parent2_index];
-					break;
-				}
-			}
-		}
-	}
-	return child;
-}
-
-//TODO: check if it's already in the population
-function insert_into_population(individual,newPopulation){
-	for(var i=0;i<newPopulation.length;i++){
-		if(arrays_equal(individual.chromosome,newPopulation[i].chromosome))
-			return false;
-	}
-	newPopulation.push(individual);
-	return true;
-}
-
-function arrays_equal(array1,array2){
-	if(array1.length != array2.length)
+function handle_worker_message_simplemax(data){
+	var resultObj = JSON.parse(data);
+	if(resultObj.act == "debug"){
+		console.log(resultObj.data);
 		return false;
-	for(var i=0;i<array1.length;i++){
-		if(array1[i] != array2[i])
-			return false;
 	}
-	return true;
-}
-
-function select_from_population(){
-	switch(config.selection){
-		case "rank":
-			var r = Math.random()*((population.length*(population.length+1))/2);
-			var sum = 0;
-			for(var i = 0;i<population.length;i++){
-				for (sum += i; sum > r; r++) return i;
-			}
-			return population.length-1;
-			break;
-		case "tournament":
-			var choices = new Array();
-			for(var i = 0;i<5;i++){
-				var rnum = Math.floor(Math.random() * population.length);
-				choices[i] = population[rnum];
-				choices[i].index = rnum;
-			}
-			if(config.fitness_order == "desc")
-				choices.sort( function (a,b) { return b.fitness-a.fitness });
-			else
-				choices.sort( function (a,b) { return a.fitness-b.fitness });
-			var r = Math.random();
-			//p = 0.5
-			if(r < 0.5){
-				//return most fit
-				return choices[choices.length-1].index;
-			}
-			//otherwise, return a random choice
-			var rnum = Math.floor(Math.random() * choices.length);
-			return choices[rnum].index;
-			break;
-		default:
-			return 1;
+	if(resultObj.act == "generation" && resultObj.gen){
+		$('#result').prepend("Best individual of generation "+resultObj.gen+": (" + resultObj.data.chromosome[0] + "*"+ resultObj.data.chromosome[1] + "*"+ resultObj.data.chromosome[2] + "*"+ resultObj.data.chromosome[3] + "*"+ resultObj.data.chromosome[4] + ")/("+ resultObj.data.chromosome[5] + "*"+ resultObj.data.chromosome[6] + "*"+ resultObj.data.chromosome[7] + "*"+ resultObj.data.chromosome[8] + "*"+ resultObj.data.chromosome[9] +") = "+resultObj.data.fitness+"<br>");
+		return true;
+	}
+	if(resultObj.act == "answer"){
+		$('#result').prepend("Answer: (" + resultObj.data.chromosome[0] + "*"+ resultObj.data.chromosome[1] + "*"+ resultObj.data.chromosome[2] + "*"+ resultObj.data.chromosome[3] + "*"+ resultObj.data.chromosome[4] + ")/("+ resultObj.data.chromosome[5] + "*"+ resultObj.data.chromosome[6] + "*"+ resultObj.data.chromosome[7] + "*"+ resultObj.data.chromosome[8] + "*"+ resultObj.data.chromosome[9] +") = "+resultObj.data.fitness+"<br>");
+		return true;
 	}
 }
 
-function one_max_fitness(chromosome){
-	var fitness = 0;
-	for(var j = 0;j<chromosome.length;j++){
-		if(chromosome[j] == '1')
-			fitness++;
+
+function handle_worker_message_tsp(data){
+	var resultObj = JSON.parse(data);
+	if(resultObj.act == "debug"){
+		console.log(resultObj.data);
+		return false;
 	}
-	return fitness;
+	if(resultObj.act == "generation"){
+		$('#result').html("Fitness: "+resultObj.data.fitness+"<br>");
+		draw_tsp(resultObj.data.chromosome);
+		return true;
+	}
+	if(resultObj.act == "answer"){
+		$('#result').html("Done: Fitness: "+resultObj.data.fitness+"<br>");
+		draw_tsp(resultObj.data.chromosome);
+		return true;
+	}
 }
 
-function simple_max_fitness(chromosome){
-	return (chromosome[0]*chromosome[1]*chromosome[2]*chromosome[3]*chromosome[4])/(chromosome[5]*chromosome[6]*chromosome[7]*chromosome[8]*chromosome[9]);
+function draw_cities(){
+	ctx.clearRect(0, 0, 900, 600);
+	ctx.fillStyle = "#000";
+	for(i in config.cities){
+		ctx.beginPath();
+		ctx.arc(config.cities[i].x/2,config.cities[i].y/2,2,0,Math.PI*2,true);
+		ctx.fill();
+	}
 }
 
-function tsp_fitness(chromosome){
-	var fitness = 0;
-	//Use config to get location of cities, and add up distances.
-	for(var j = 1;j<chromosome.length;j++){
-		//increase fitness by distance from j-1 to j
-		var xd = config.cities[chromosome[j-1]].x - config.cities[chromosome[j]].x;
-		var yd = config.cities[chromosome[j-1]].y - config.cities[chromosome[j]].y;
-		
-		fitness+=Math.floor(Math.sqrt(xd*xd + yd*yd) + 0.5);
+function draw_tsp(path){
+	draw_cities();
+	ctx.fillStyle = "#0F0";
+	ctx.beginPath();
+	ctx.arc(config.cities[path[0]].x/2,config.cities[path[0]].y/2,2,0,Math.PI*2,true);
+	ctx.fill();
+	ctx.strokeStyle = "#666";
+	ctx.beginPath();
+	ctx.moveTo(config.cities[path[0]].x/2,config.cities[path[0]].y/2);
+	for(var i=0;i<path.length;i++){
+		ctx.lineTo(config.cities[path[i]].x/2,config.cities[path[i]].y/2);
 	}
-	//return to first city
-	var xd = config.cities[chromosome[chromosome.length-1]].x - config.cities[chromosome[0]].x;
-	var yd = config.cities[chromosome[chromosome.length-1]].y - config.cities[chromosome[0]].y;
-		
-	fitness+=Math.floor(Math.sqrt(xd*xd + yd*yd) + 0.5);
-	return fitness;
+	ctx.lineTo(config.cities[path[0]].x/2,config.cities[path[0]].y/2);
+	ctx.stroke();  
 }
 
+//pause the game
+function stop(){
+	var message = new Object();
+	message.act = "pause";
+	worker.postMessage(JSON.stringify(message));
+}
 
-//randomly generate a string
-function generate_chromosome() {
-	var randomchromosome = new Array();
-	if(config.unique_chromosomes){
-		var available_chars = config.chars.slice();
-		while(available_chars.length > 0){
-			var rnum = Math.floor(Math.random() * available_chars.length);
-			randomchromosome = randomchromosome.concat(available_chars.splice(rnum,1));
-		}
-	}else{
-		for (var i=0; i<config.chromosome_length; i++) {
-			var rnum = Math.floor(Math.random() * config.chars.length);
-			randomchromosome.push(config.chars[rnum]);
-		}
-	}
-	return randomchromosome;
+function test_crossover(){
+	stop();
+	$('#result').empty();
+	
+	worker.onmessage = function(event) {
+		var resultObj = JSON.parse(event.data);
+		$('#result').append("Order Crossover: "+resultObj.data.a1);
+		$('#result').append("<br>Position-based crossover: "+resultObj.data.a2);
+	};
+	var message = new Object();
+	message.act = "test_crossover";
+	message.data = new Object();
+	var string = prompt("Enter values for parent 1 separated by a comma (,).\nFor Example: 1,2,3,4");
+	if(!string)
+		return false;
+	message.data.parent1 = string.split(',');
+	string = prompt("Enter values for parent 2 separated by a comma (,).\nFor Example: 1,2,3,4");
+	if(!string)
+		return false;
+	message.data.parent2 = string.split(',');;
+	worker.postMessage(JSON.stringify(message));
 }
